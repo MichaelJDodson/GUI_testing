e6057dff0f200a0c435a3ca6f8f42950a4a9b28a
Michael Dodson <michaeljdodson42@gmail.com>
2024-03-02 20:49:30 -0500

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("gui_test")]
+async fn main() {
+    
+    loop {
+        clear_background(WHITE);
+
+        
+        
+
+        next_frame().await;
+    }
+
+}
